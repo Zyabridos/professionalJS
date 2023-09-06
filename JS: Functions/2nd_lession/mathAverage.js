ca9bce5153e6152e7c866eac15d0945e34131442
Zyabridos <zyabrina95@gmail.com>
2023-09-06 17:32:53 +0200

    second lession is completed

---
+++ JS: Functions/2nd_lession/mathAverage.js
@@ -0,0 +1,29 @@
+// Реализуйте и экспортируйте по умолчанию функцию,
+// которая возвращает среднее арифметическое всех переданных аргументов.
+// Если функции не передать ни одного аргумента, то она должна вернуть null.
+
+import _ from 'lodash';
+
+function average ( ...numbers) {
+  const count = numbers.length;
+
+  if (count === 0) {
+    return null;
+  }
+
+  return _.sum(numbers) / count;
+}
+
+export default average
+
+// average(0); // 0
+// average(0, 10); // 5
+// average(-3, 4, 2, 10); // 3.25
+// average(); // null
+
+console.log(average(0)); // 0
+console.log(average(0, 10)); // 5
+console.log(average(-3, 4, 2, 10)); // 3.25
+console.log(average(-3, -2, -2, -12));
+console.log(average(18, 4, 2, 10));
+console.log(average()); // null
