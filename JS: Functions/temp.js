68cd38adc8f712d0e665556060597c962124e303
Zyabridos <zyabrina95@gmail.com>
2023-09-07 15:16:23 +0200

    reduce() lession is completed

--- JS: Functions/temp.js
+++ JS: Functions/temp.js
@@ -1,3 +1,20 @@
-const value = 777;
-const func = (value) => value + 1;
-console.log(func(5));
+const users = [
+  { name: 'Petr', age: 4 },
+  { name: 'Igor', age: 19 },
+  { name: 'Ivan', age: 4 },
+  { name: 'Matvey', age: 16 },
+];
+
+// предварительно подготовим функцию-обработчик
+const cb = (acc, user) => {
+  if (!Object.hasOwn(acc, user.age)) {
+    acc[user.age] = [];
+  }
+  acc[user.age].push(user.name);
+  return acc; // обязательно вернуть!
+};
+
+// Начальное значение аккумулятора – пустой объект
+const usersByAge = users.reduce(cb, {});
+
+console.log(usersByAge);
