ad6df722d03b9ab02d178ccb80f7d12425e0536b
Zyabridos <zyabrina95@gmail.com>
2023-10-21 15:34:05 +0200

    moduleTesting task is completed

---
+++ JS: Automatizated testing/moduleTesting/__tests__/validator.test.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import ( check) from '../src/index.js';
+
+const makeValidator = getImplementation();
+
+test('validator', () => {
+  const validator = makeValidator();
+  expect(validator.isValid('value')).toBe(true);
+
+  validator.addCheck(_.isNumber);
+  expect(validator.isValid('string')).toBe(false);
+
+  validator.addCheck((v) => v > 10);
+  expect(validator.isValid(100)).toBe(true);
+
+  validator.addCheck((v) => v % 2 === 0);
+  expect(validator.isValid(11)).toBe(false);
+  expect(validator.isValid(12)).toBe(true);
+  expect(validator.isValid(8)).toBe(false);
+});
