a2fc6bdbf550a7fafb342cf298c34d7cf22d1e31
Zyabridos <zyabrina95@gmail.com>
2023-10-26 19:29:35 +0200

    tests for middle point

--- JS: Abstraction with the help of data/middlePointOfRTwoPoints/__tests__/middlePointOfTwoPoints.js
+++ JS: Abstraction with the help of data/middlePointOfRTwoPoints/__tests__/middlePointOfTwoPoints.js
@@ -1,9 +1,15 @@
-import { strict as assert, equal } from 'node:assert';
 import getMidpoint from '../src/middlePointOfTwoPoints.js';
 
-const point1 = { x: 0, y: 0 };
-const point2 = { x: 4, y: 4 };
-const point3 = getMidpoint(point1, point2);
+test('getMidpoint', () => {
+  const point1 = { x: 0, y: 0 };
+  const point2 = { x: 4, y: 4 };
+  expect(getMidpoint(point1, point2)).toStrictEqual({ x: 2, y: 2 });
+});
 
-assert.equal(getMidpoint(point1, point2), { x: 2, y: 2 });
-console.log(point3); // => { x: 2, y: 2 };
+test('getMidpoint2', () => {
+  const point1 = { x: -1, y: 10 };
+  const point2 = { x: 0, y: -3 };
+  expect(getMidpoint(point1, point2)).toStrictEqual({ x: -0.5, y: 3.5 });
+});
+
+// NODE_OPTIONS=--experimental-vm-modules npx jest
